648cc84c4c24409f9a93e497e6545338f35b089a
AdamASchulte <adamaschulte@yahoo.com>
2023-09-05 19:41:00 -0500

    Create main.java

---
+++ DiningPhilosophers/src/main.java
@@ -0,0 +1,79 @@
+class main {
+
+  public static void main(String args[]) {
+    Chopstick[] chopsticks = new Chopstick[args[0]];
+    for (int j = 0; j < args[0]; j++) {
+      chopsticks[i] = new Chopstick();
+    }
+
+    Thread[] philosophers = new Thread[args[0]];
+    philosophers[0] =
+      new Thread(new Philosopher(0, chopsticks[args[0]], chopsticks[0]));
+    for (int i = 1; i < philosophers.length; i++) {
+      philosophers[i] =
+        new Thread(new Philosopher(i, chopsticks[i - 1], chopsticks[i]));
+      threads[i].start();
+    }
+  }
+}
+
+class Chopstick {
+
+  private bool isTaken = false;
+
+  public Chopstick() {}
+}
+
+class Philosoper implements Runnable {
+
+  private int position;
+  private Chopstick leftChopstick;
+  private Chopstick rightChopstick;
+  private boolean hungry = true;
+
+  public Philosopher(int pos, Chopstick left, Chopstick right) {
+    position = pos;
+    leftChopstick = left;
+    rightChopstick = right;
+  }
+
+  public void run() {}
+}
+
+// class Monitor {
+
+//   synchronized void wait() {}
+// }
+
+public class MyMonitor {
+
+  private int flag = 0;
+
+  public synchronized void myThreadA() {
+    while (flag == 0) {
+      try {
+        wait();
+      } catch (InterruptedException e) {
+        System.out.println("InterruptedException caught");
+      }
+      flag = 1;
+      System.out.println("ThreadA is done");
+    }
+  }
+
+  public synchronized void myThreadB() {
+    while (flag == 1) {
+      try {
+        wait();
+      } catch (InterruptedException e) {
+        System.out.println("InterruptedException caught");
+      }
+      flag = 0;
+      System.out.println("ThreadB is done");
+    }
+  }
+
+  public synchronized void signalOtherThread() {
+    notify();
+  }
+}
